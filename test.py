4998121bd73a538ddd8f2ce9936830febc25b8c8
Uprant choudhary <uprant_choudhary@outlook.com>
2025-07-14 23:31:34 +0530

    added readme and changed test

--- test.py
+++ test.py
@@ -1,8 +1,8 @@
 
-print(' chip chip')
-print(' phip phip')
-print(' vhip vhip')
-print(' hhip hhip')
+print('chucky chucky chuck chuck')
+print(' phucky phucky phuck phuck')
+print(' vhucky vhucky vhuck vhuck')
+print(' prip prip')
 
 print(' this is test-file.py')
 print('this is in extended branch')
